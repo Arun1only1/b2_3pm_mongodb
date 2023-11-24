b3dd1ce44f28f08bce4b9faaa2340ff359af144c
Arun1only1 <arun1nly1@gmail.com>
2023-11-24 16:28:09 +0545

    assignment

--- c.assignment1.mongodb.js
+++ c.assignment1.mongodb.js
@@ -1,5 +1,7 @@
 use("b2_3pm");
 
+// ?read operations
+
 //?Find movies whose status is "Ended"
 // db.movies.find({status:"Ended"})
 
@@ -33,14 +35,18 @@ use("b2_3pm");
 
 // ?find movies whose genre includes Action and Crime
 // !db.movies.find({genres:"Action",genres:"Crime"},{name:1,genres:1})
-// db.movies.find()
+// db.movies.find({$and:[{genres:"Action"},{genres:"Crime"}]})
+
+// db.movies.find({genres:{$all:["Action","Crime"]}})
 
 // ! find movies whose genre size is 2
+// db.movies.find({genres:{$size:2}})
 
 // ? find movies whose language is not English
 // db.movies.find({language:{$ne:"English"}})
 
 // ! find movies whose summary includes "Pacific"
+// db.movies.find({summary:{$regex:"Pacific",$options:"i"}},{name:1, summary:1})
 
 // ? find movies whose rating average is either 6 or 6.5 or
 // ?9 or 8 or 8.5 or 8.6 or 7.7 or 6.1 or 7.8
@@ -48,3 +54,32 @@ use("b2_3pm");
 // db.movies.find({$or:[{"rating.average":6},{"rating.average":6.5},{"rating.average":9},{"rating.average":8},{"rating.average":8.5},{"rating.average":8.6},{"rating.average":7.7},{"rating.average":6.1},{"rating.average":7.8}]}).count()
 
 // db.movies.find({"rating.average":{$in:[6,6.5,9,8,8.5,8.6,7.7,6.1,7.8]}}).count()
+
+// ?find students whose hobby is both Gaming and Singing
+
+// db.students.find({$and:[{hobbies:"Gaming"},{hobbies:"Singing"}]})
+
+// db.students.find({hobbies:{$all:["Gaming","Singing"]}})
+
+// ? find students whose hobby is neither Swimming nor Dancing
+
+// db.students.find({$nor:[{hobbies:"Swimming"},{hobbies:"Dancing"}]})
+
+// db.students.find({hobbies:{$nin:["Swimming","Dancing"]}})
+
+// ?find students who have three hobbies
+// db.students.find({hobbies:{$size:3}})
+// db.students.find({hobbies:{$not:{$size:3}}})
+
+// ?find students whose math score is greater than 50
+
+// db.students.find({scores:{$elemMatch:{sub:"Math",point:{$gt:50}}}})
+
+// ?find students whose score in any subject is greater than 70
+
+// db.students.find({"scores.point":{$gt:70}})
+
+// !db.students.find({$expr:{$gt:["$scores.point",70]}})
+
+// ? find students whose primary id is "656070d2b16daaa31169a51b"
+// db.students.find({ _id: ObjectId("656070d2b16daaa31169a51b") });
