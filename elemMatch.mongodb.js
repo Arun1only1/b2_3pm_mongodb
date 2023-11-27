d930bede8263ddb32af27101087fa3d5fc5ff945
Arun1only1 <arun1nly1@gmail.com>
2023-11-27 17:38:50 +0545

    update operations

---
+++ elemMatch.mongodb.js
@@ -0,0 +1,24 @@
+use("b2_3pm");
+
+// db.sizes.insertMany([
+//     {
+//         name:"adidas",
+//         size:[41,39,42]
+//     },
+//     {
+//         name:"erke",
+//         size:[38,39,42]
+//     },{
+//         name:"Anta",
+//         size:[38,39,40]
+//     }
+
+// ])
+
+// db.sizes.find()
+
+// shoes size greater than 39 and less than 42
+
+// db.sizes.find({
+//     size:{$elemMatch:{$gt:39,$lt:42}}
+// })
---
+++ h.update_array.mongodb.js
@@ -0,0 +1,150 @@
+use("b2_3pm");
+
+// db.friends.updateOne({firstName:"Nikita"},{
+//     $set:{
+//         scores:[85,95,75]
+//     }
+// })
+
+// db.friends.updateOne({},{
+//     $rename:{
+//         firstName:"name"
+//     }
+// })
+
+// ?$push => appends value to array
+// db.friends.updateOne({name:"Nikita"},{
+//     $push:{
+//         scores:65
+//     }
+// })
+
+// ? $push with $each to add multiple values to array
+// db.friends.updateOne({name:"Nikita"},{
+//     $push:{
+//         scores:{$each:[45,55,35,25,65,85,95]}
+//     }
+// })
+
+// ?$pop => removes first(-1) or last item(1) from array
+// db.friends.updateOne({name:"Nikita"},{
+//     $pop:{
+//         scores:1
+//     }
+// })
+
+// db.friends.updateOne({name:"Nikita"},{
+//     $pop:{
+//         scores:-1
+//     }
+// })
+
+// ? $pull => removes item from array based on condition
+// ?and also removes specified value
+
+// db.friends.updateOne({name:"Nikita"},{
+//     $pull:{
+//         scores:{$lt:70}
+//     }
+// })
+
+// db.friends.updateOne({name:"Nikita"},{
+//     $pull:{
+//         scores:45
+//     }
+// })
+
+//? $pullAll => removes multiple values (no condition can be applied)
+
+// db.friends.updateOne({name:"Nikita"},{
+//     $pullAll:{
+//         scores:[75,25,35]
+//     }
+// })
+
+// db.friends.updateOne({name:"Nikita"},{
+//     $set:{
+//         scores:[]
+//     }
+// })
+
+//? $addToSet => uniqueness of array
+
+// db.friends.updateOne({
+//     name:"Nikita"
+// },
+// {
+//     $addToSet:{
+//         scores:{$each:[10,20]}
+//     }
+// })
+
+//TODO: push values to starting of array
+
+// ?$
+// db.friends.updateOne({name:"Nikita",scores:45},{
+//     $set:{
+//         "scores.$":46
+//     }
+
+// })
+
+// db.friends.updateOne({name:"Nikita",hobbies:"Hiking"},{
+// $set:{
+//     "hobbies.$":"Trekking"
+// }
+// })
+
+// db.friends.updateOne({name:"Nikita","sportData.title":"Basketball"},{
+//     $set:{
+//         "sportData.$.title":"Badminton"
+//     }
+// })
+
+// db.friends.updateOne({
+//     name:"Nikita",
+//     "sportData.title":"Football"
+// },
+// {
+//     $inc:{
+//         "sportData.$.frequency":5
+//     }
+
+// })
+
+// db.friends.updateOne({firstName:"Dipesh","sportData.title":"Tennis"},{
+//     $set:{
+//         "sportData.$.title":"Long-Tennis",
+//         "sportData.$.frequency":5
+//     }
+// })
+
+// ?$[]=>
+// Acts as a placeholder to update all elements in
+//  an array for the documents that match the query condition.
+
+// db.friends.updateOne({firstName:"Dipesh"},{
+//     $set:{
+//         "hobbies.$[]":"Cricket"
+//     }
+// })
+
+// db.friends.updateOne({
+//     firstName:"Dipesh"
+// },{
+
+//     $set:{
+//        "sportData.$[].frequency":7
+//     }
+// })
+
+//? $[element]
+// db.friends.updateOne({name:"Nikita"},{
+//     $set:{
+//         "scores.$[element]":100
+//     }
+// },
+// {
+//     arrayFilters:[{element:{$lt:50}}]
+// })
+db.friends.find();
